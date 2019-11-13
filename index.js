335cb24ad58cfb976e8e32d4f10526b9f45a834c
sk41TV <kaihschuhmacher@gmail.com>
2019-11-13 14:08:53 +0100

    js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function setColorBySeason() {
-    var d = new Date();
-    switch(d.getMonth()) {
-        case 11, 0, 1: document.getElementById("bg").style.background = "#348cb2"; break;
-        case 2, 3, 4: document.getElementById("bg").style.background = "#00CD00"; break;
-        case 5, 6, 7: document.getElementById("bg").style.background = "#e7e307"; break;
-        case 8, 9, 10: document.getElementById("bg").style.background = "#ebb609"; break;
-        default: break;
-    }
-};
+var d = new Date();
+switch(d.getMonth()) {
+    case 11, 0, 1: document.getElementById("bg").style.background = "#348cb2"; break;
+    case 2, 3, 4: document.getElementById("bg").style.background = "#00CD00"; break;
+    case 5, 6, 7: document.getElementById("bg").style.background = "#e7e307"; break;
+    case 8, 9, 10: document.getElementById("bg").style.background = "#ebb609"; break;
+    default: break;
+}
